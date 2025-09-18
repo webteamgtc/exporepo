3e6636183dd1d5ba208ccaf4b018cc30fcbf7774
adeel <adeelcomsats070@gmail.com>
2025-09-19 02:30:07 +0500

    Fixed : remove dir

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ["gtcfx-bucket.s3.ap-southeast-1.amazonaws.com"],
   },
