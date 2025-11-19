10062385374986f2a8dc9fe480182ea2483aa941
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-11-19 18:11:15 +0400

    sdsdas

--- src/app/[locale]/luckydraw-falcon-registration/page.jsx
+++ src/app/[locale]/luckydraw-falcon-registration/page.jsx
@@ -6,10 +6,13 @@ import StepsToWinSection from "./StepsToWinSection";
 import PrizeProgression from "./PrizeProgression";
 import CountdownHero from "./CountdownHero";
 import Footer from "./Footer";
+import Meta from "../components/Meta";
 
 export default function LuckyDrawFalconRegistration() {
   return (
+
     <div className="min-h-screen bg-[#0F143A] text-white">
+      <Meta title="Join the Lucky Draw Falcon Registration!" description="Register now for a chance to win exciting prizes in our Lucky Draw Falcon event. Don't miss out on this opportunity!" />
       <HeroSection />
       <StepsToWinSection />
       <PrizeProgression />
